a5f86cef900aa668c6fffa79484b3d55bf5e85eb
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-06 14:01:22 -0800

    start proxy server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const path = require('path');
 
+// const headerSidebar = require('../client/header-sidebar-service/server/index.js');
+// const instructors = require('../client/instructors-service/server/index.js');
+// const feedback = require('../client/Student-Feedback/server/index.js');
+// const students = require('../client/students-also-bought-service/server/server.js');
+
+const PORT = 3000;
+const app = express();
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}/`);
+});
+app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
